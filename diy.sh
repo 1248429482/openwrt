6f544ba77b26f9fea3b6f1ff45ca87290fe6d303
1248429482 <1248429482@qq.com>
2020-03-14 20:54:17 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,6 +9,7 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 #主题
+cd openwrt
 cd package/lean  
 rm -rf luci-theme-argon  
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  
